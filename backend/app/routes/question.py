da675d99f3d53d9c3219e7a33b18d7264d8f5890
ZiwenZhang <a60595677@gmail.com>
2024-11-01 17:44:22 -0400

    finished question skeleton, havent test yet

--- backend/app/routes/question.py
+++ backend/app/routes/question.py
@@ -38,5 +38,64 @@ def get_question_by_id(id):
         return jsonify({"error": "Question not found"})
 
 
-# @bp.route("/", methods=["POST"])
-# def create_question():
+@bp.route("/", methods=["POST"])
+def create_question():
+    if not current_user.is_authenticated:
+        return jsonify({"error": "not authenticated"})
+
+    data = request.get_json()
+    #? validate check
+    # if not data:
+    #     return jsonify({"error": "Content is required"})
+    new_question = Question(
+        user_id=current_user.id,
+        content = data['content']
+    ##?more stuff?
+    )
+    db.session.add(new_question)
+    db.seesion.commit()
+    return jsonify({
+            "id": new_question.id,
+            "user_id": new_question.user_id,
+            "created_at": new_question.created_at,
+            "updated_at": new_question.updated_at
+    })
+
+
+@bp.route("/<int:id>", methods=["PUT"])
+def edit_question(id):
+    if not current_user.is_authenticated:
+        return jsonify({"error": "not authenticated"})
+    question = Question.query.get(id)
+    if not question:
+        return jsonify({"error": "Question not found"})
+    if question.user_id != current_user.id:
+        return jsonify({"error": "Unauthorized to edit this question"})
+    data = request.get_json()
+    # ? validate check
+    # if not data:
+    #     return jsonify({"error": "Content is required"})
+    question.content = data['content']
+    db.session.commit()
+
+    return jsonify({
+        "question": {
+            "id": question.id,
+            "user_id": question.user_id,
+            "content": question.content,
+        }
+    }), 200
+
+@bp.route("/<int:id>", methods=["DELETE"])
+def delete_question(id):
+    if not current_user.is_authenticated:
+        return jsonify({"error": "not authenticated"})
+    question = Question.query.get(id)
+    if not question:
+        return jsonify({"error": "Question not found"})
+    if question.user_id != current_user.id:
+        return jsonify({"error": "Unauthorized to delete this question"})
+    db.session.delete(question)
+    db.session.commit()
+    return jsonify({"message": "Question deleted"})
+

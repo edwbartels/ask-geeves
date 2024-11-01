1637ccb6dd34eabd0d3db95c6f7e76241325578e
edwbartels <edwbartels@gmail.com>
2024-11-01 16:31:38 -0400

    first migrations

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,6 +4,7 @@ from flask_login import LoginManager
 from .models.db import db
 from .models.user import User
 from .routes import session
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,10 +12,16 @@ app.config.from_object(Config)
 app.register_blueprint(session.bp)
 db.init_app(app)
 
+migrate = Migrate(app, db)
+
+
 login = LoginManager(app)
-login.login_view = 'session.login'
+login.login_view = "session.login"
+
 
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
-'hi there'
+
+
+"hi there"

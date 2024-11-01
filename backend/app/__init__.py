f9d136bebe47e8c3c8ec811e9a28927e47c968da
ZiwenZhang <a60595677@gmail.com>
2024-11-01 17:49:16 -0400

    matching up dev

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,19 +3,25 @@ from .config import Config
 from flask_login import LoginManager
 from .models.db import db
 from .models.user import User
-from .routes import session,user
+from .routes import session
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 app.register_blueprint(session.bp)
-app.register_blueprint(user.bp)
 db.init_app(app)
 
+migrate = Migrate(app, db)
+
+
 login = LoginManager(app)
-login.login_view = 'session.login'
+login.login_view = "session.login"
+
 
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
-'hi there'
+
+
+"hi there"
--- backend/app/config.py
+++ backend/app/config.py
@@ -4,4 +4,5 @@ import os
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
     FLASK_ENV = os.environ.get("FLASK_ENV")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///dev.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False

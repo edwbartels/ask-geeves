b043af1d904d078bde51b75a07ec81301c445702
ZiwenZhang <a60595677@gmail.com>
2024-10-31 21:05:18 -0400

    edit init.py added session

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,19 @@
 from flask import Flask
 from .config import Config
-
+from flask_login import LoginManager
+from .models.db import db
+from .models.users import User
+from .routes import session
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+app.register_blueprint(session.bp)
+db.init_app(app)
+
+login = LoginManager(app)
+login.login_view = 'session.login'
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))

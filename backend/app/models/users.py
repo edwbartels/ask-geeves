53802339d859e49432992f0ca39cfce8b9c759cf
edwbartels <edwbartels@gmail.com>
2024-10-31 20:02:37 -0400

    models

---
+++ backend/app/models/users.py
@@ -0,0 +1,41 @@
+from .db import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime, timezone
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    username = db.Column(db.String(24), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    _hashed_password = db.Column(db.String(255), nullable=False)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(
+        db.DateTime(timezone=True), nullable=False, default=datetime.now(timezone.utc)
+    )
+    updated_at = db.Column(
+        db.DateTime(timezone=True), nullable=False, default=datetime.now(timezone.utc)
+    )
+    questions = db.relationship(
+        "Question", backref="user", cascade="all, delete-orphan"
+    )
+    answers = db.relationship("Answer", backref="user", cascade="all, delete-orphan")
+    comments = db.relationship("Comment", backref="user", cascade="all, delete-orphan")
+    saves = db.relationship("Save", backref="user", cascade="all, delete-orphan")
+
+    @property
+    def password(self):
+        raise AttributeError("Password is not readable")
+
+    @password.setter
+    def password(self, password):
+        self._hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self._hashed_password, password)
+
+    def __repr__(self):
+        return f"<User {self.username} {self.email}>"

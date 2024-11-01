e45b0252cead635c0110397c84696633c0c5f209
ZiwenZhang <a60595677@gmail.com>
2024-11-01 15:12:17 -0400

    lunch time, working on questions

---
+++ backend/app/routes/question.py
@@ -0,0 +1,40 @@
+from flask import Blueprint,jsonify
+from flask_login import current_user
+from ..models.question import Question
+from ..models.db import db
+
+bp = Blueprint("question", __name__, url_prefix="/questions")
+
+
+@bp.route("/", methods=["GET"])
+def get_all_questions():
+    allQuestions = Question.query.all()
+    questions_list = []
+    for question in allQuestions:
+        eachQuestion = {
+            "id": question.id,
+            "user_id": question.user_id,
+            "content": question.content,
+            "created_at": question.created_at,
+            "updated_at": question.updated_at,
+        }
+        questions_list.append(eachQuestion)
+    return jsonify({"Questions": questions_list})
+
+
+@bp.route("/<int:id>", methods=["GET"])
+def get_question_by_id(id):
+    question = Question.query.get(id)
+    if question:
+        question_data = {
+            "id": question.id,
+            "user_id": question.user_id,
+            "content": question.content,
+            "created_at": question.created_at,
+            "updated_at": question.updated_at
+        }
+        return jsonify({"Question": question_data})
+    else:
+        return jsonify({"error": "Question not found"})
+
+
--- backend/app/routes/session.py
+++ backend/app/routes/session.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,flash,jsonify
+from flask import Blueprint,jsonify,request
 from flask_login import current_user, login_user, logout_user
 from ..utils.forms import LoginForm
 from ..models.user import User
@@ -21,30 +21,36 @@ def get_current_user():
 @bp.route("/", methods=["POST"])
 def login():
     if current_user.is_authenticated:
-        return jsonify({"message": "already logged in bro"}), 200
+        return jsonify({"message": "already logged in bro"})
     #should hide login button
     
-    form = LoginForm()
-    if form.validate_on_submit():
-        username = form.username.data
-        user = User.query.filter(User.username == username).first()
-        if not user or not user.check_password(form.password.data):
-            flash("wrong!")
-            return jsonify({"error": "Invalid username or password"}), 401
-        login_user(user)
-        return jsonify({"user": {
-            "id": user.id,
-            "username": user.username,
-            "email": user.email,
-            "first_name": user.first_name,
-            "last_name": user.last_name
-        }}), 200
-    return jsonify({"error": "something wrong with form input"})
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "something wrong with request format"})
+
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        return jsonify({"error": "Username and password are required"}), 400
+
+    user = User.query.filter_by(username=username).first()
+    if not user or not user.check_password(password):
+        return jsonify({"error": "Invalid username or password"}), 401
+
+    login_user(user)
+    return jsonify({"user": {
+        "id": user.id,
+        "username": user.username,
+        "email": user.email,
+        "first_name": user.first_name,
+        "last_name": user.last_name
+    }}), 200
 
 @bp.route('/logout', methods=["POST"])
 def logout():
     if not current_user.is_authenticated:
-        return jsonify({"error": "No user logged in"})
-        #should hide logout button if no current user
+        return jsonify({"error": "No user logged in"}), 400
+
     logout_user()
-    return jsonify({"message":"success"}), 200
+    return jsonify({"message": "success"}), 200

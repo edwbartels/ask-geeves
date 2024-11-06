76dafc1114a3a8d2bdeb1959f5ec2149a06f37ee
edwbartels <edwbartels@gmail.com>
2024-11-06 14:56:18 -0500

    current user questions pagination

--- backend/app/routes/question.py
+++ backend/app/routes/question.py
@@ -9,6 +9,7 @@ from ..utils.decorator import (
     question_exist_check,
     question_ownership_check,
 )
+from sqlalchemy.orm import noload, lazyload, load_only
 
 bp = Blueprint("question", __name__, url_prefix="/api/questions")
 
@@ -23,6 +24,10 @@ SORT_ORDER_MAP = {"asc": asc, "desc": desc}
 
 @bp.route("/", methods=["GET"])
 def get_all_questions():
+    # all_questions = Question.query.options(noload(Question.saves),noload(Question.answers),noload(Question.comments)).all()
+    # all_questions = Question.query.options(lazyload(Question.answers)).all()
+    # all_questions = Question.query.options(load_only(Question.id, Question.title)).all()
+
     page, per_page, sort_by, order = (
         request.args.get(key, default, type=typ)
         for key, default, typ in [
@@ -46,7 +51,7 @@ def get_all_questions():
         return jsonify({"message": "No questions found"}), 404
     questions_list = []
     for question in paginated_questions:
-        eachQuestion = question.to_dict()
+        eachQuestion = question.to_dict(homepage=True)
         questions_list.append(eachQuestion)
     # for question in all_questions:
     #     eachQuestion = question.to_dict()
@@ -73,9 +78,35 @@ def get_question_by_id(question_id):
 @bp.route("/current", methods=["GET"])
 @login_check
 def get_questions_by_current_user():
+    page, per_page, sort_by, order = (
+        request.args.get(key, default, type=typ)
+        for key, default, typ in [
+            ("page", 1, int),
+            ("per_page", 15, int),
+            ("sort_by", "created_at", str),
+            ("order", "desc", str),
+        ]
+    )
+
+    sort_column = SORT_BY_MAP.get(sort_by, Question.created_at)
+    sort_order = SORT_ORDER_MAP.get(order, desc)
+
+    questions = Question.query.filter(user_id=current_user.id).order_by(
+        sort_order(sort_column)
+    )
+    paginated_questions = questions.paginate(page=page, per_page=per_page)
+    total_pages = paginated_questions.pages
+
     user_questions = Question.query.filter_by(user_id=current_user.id).all()
     questions_list = [question.to_dict() for question in user_questions]
-    return jsonify({"questions_owned": questions_list}), 200
+    return jsonify(
+        {
+            "page": page,
+            "size": per_page,
+            "total_pages": total_pages,
+            "questions_owned": questions_list,
+        }
+    ), 200
 
 
 @bp.route("/", methods=["POST"])

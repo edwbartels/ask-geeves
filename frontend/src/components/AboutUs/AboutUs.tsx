249a4b9e9362edbd5ab022a62226f5c41ae3286e
TinaK92 <tinakonopko@gmail.com>
2024-11-12 21:23:40 -0500

    new commit comment component started

--- frontend/src/components/AboutUs/AboutUs.tsx
+++ frontend/src/components/AboutUs/AboutUs.tsx
@@ -3,14 +3,21 @@ import './AboutUs.css'
 export const AboutUs = () => {
     return (
         <div className='about-us-div'>
-            <div><h1 className='about-us-title'>About Us</h1></div>
             <div>
-                <p>
-                    agn;kajdfngl;kajndfgjnfkgnjdfnjgans;dfkjna;skjdnf;sdjknf;kjdfngkjanfdg;kjadnf ;akjndf;kajsndf;kjnsd;fkj NDFJKSDFNKA JNFAKSJDNF ;KJDNFNGksmdf;lgkm; s;dflkgmj ;lkdf;klmg ;ldfmg;sdf;lkjfghskljdfhg fdgkjsdfhglkjshdfglkj hdsjkfhgl skjdfhglkf
-                </p>
+                <h1 className='about-us-title'>About Us</h1>
             </div>
-            <div className='about-us-button-div'>
-                <button className='about-us-button'>Submit</button>
+            <hr/>
+            <div>
+                <h3 className='mission title'>Our Mission</h3>
+                <p className='mission-statement'>
+                    At Ask Geeves, our mission is to create a collaborative 
+                    space where curious minds can ask questions, share knowledge, 
+                    and empower each other through thoughtful, insightful answers. 
+                    We believe in the power of community-driven learning, where 
+                    every question is valued, every answer is an opportunity to 
+                    teach, and every user contributes to building a smarter, more 
+                    informed world.
+                </p>
             </div>
         </div>
     )

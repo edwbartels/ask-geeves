47635f75253e07dfe6d24f36afcc6f6b8e0fe37e
TinaK92 <tinakonopko@gmail.com>
2024-11-14 12:50:56 -0500

    no changes

--- backend/app/models/join_tables.py
+++ backend/app/models/join_tables.py
@@ -2,8 +2,7 @@ from .db import db, add_prefix_for_prod
 import os
 from .base_models import Base
 
-schema = os.getenv("SCHEMA")
-#  if os.getenv("FLASK_ENV") == "production" else None
+schema = os.getenv("SCHEMA") if os.getenv("FLASK_ENV") == "production" else None
 
 question_tags = db.Table(
     "question_tags",

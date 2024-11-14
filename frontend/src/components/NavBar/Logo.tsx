9cc0f543994d07afd49bfb9fcbaf5ad2865b61b8
Josh Wang <joshwanf@users.noreply.github.com>
2024-11-14 11:21:16 -0500

    Update Logo.tsx
    Use Link instead of NavLink

--- frontend/src/components/NavBar/Logo.tsx
+++ frontend/src/components/NavBar/Logo.tsx
@@ -1,12 +1,12 @@
-import { NavLink } from "react-router-dom"
+import { Link } from "react-router-dom"
 import "./Logo.css"
 
 export const Logo = () => {
   return (
     <div className="container">
-      <NavLink to="/" className="sign">
+      <Link to="/" className="sign">
       <span className="fast-flicker">A</span>sk<span className="flicker">&nbsp;G</span>eeves
-      </NavLink>
+      </Link>
     </div>
   )  
 }

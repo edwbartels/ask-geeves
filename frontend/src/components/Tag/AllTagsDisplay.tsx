9aa4f5cd53658b1038d12f38e1fd48c74605896d
joshwanf <17016446+joshwanf@users.noreply.github.com>
2024-11-15 15:28:49 -0500

    added syntax highlighting to posts. fixed some question/answer seeders to have markdown format

--- frontend/src/components/Tag/AllTagsDisplay.tsx
+++ frontend/src/components/Tag/AllTagsDisplay.tsx
@@ -3,7 +3,6 @@ import { useAppSelector, useAppDispatch } from "../../app/hooks"
 import { selectTags, getAllTags } from "../../features/tagsSlice"
 import { Tag } from "../Tag/Tag"
 
-
 export const AllTags = () => {
   const dispatch = useAppDispatch()
   const [isLoadedTags, setIsLoadedTags] = useState(false)
@@ -13,14 +12,14 @@ export const AllTags = () => {
   }
   const tagIds = Object.keys(useAppSelector(selectTags))
   return (
-      <div>
-        <h1 className="tag-title">All Tags</h1>
-        <hr className="tag-line" />
-        <div className="all-tags">
-          {tagIds.map(tagId => (
-            <Tag key={tagId} tagId={Number(tagId)} />
-          ))}
-        </div>
+    <div>
+      <h1 className="tag-title">All Tags</h1>
+      <hr className="tag-line" />
+      <div className="all-tags-list">
+        {tagIds.map(tagId => (
+          <Tag key={tagId} tagId={Number(tagId)} />
+        ))}
       </div>
+    </div>
   )
 }

cb1ec3e0faff1b282c0a9a7dd1a20e8e9a2e84ad
joshwanf <17016446+joshwanf@users.noreply.github.com>
2024-11-15 11:09:30 -0500

    styled tag tile list in all tags page

--- frontend/src/components/AllTags/AllTags.tsx
+++ frontend/src/components/AllTags/AllTags.tsx
@@ -3,6 +3,8 @@ import { useAppSelector, useAppDispatch } from "../../app/hooks"
 import { selectTags, getAllTags } from "../../features/tagsSlice"
 import { Tag } from "../Tag/Tag"
 import { TagTile } from "../Tag/TagTile"
+import "../tag/devicon-base.css"
+
 export const AllTags = () => {
   const dispatch = useAppDispatch()
   const [isLoadedTags, setIsLoadedTags] = useState(false)
@@ -14,10 +16,7 @@ export const AllTags = () => {
   return (
     <div className="all-tags">
       {tagIds.map(tagId => (
-        <div>
-          <TagTile />
-          <Tag key={tagId} tagId={Number(tagId)} />
-        </div>
+        <TagTile key={tagId} tagId={Number(tagId)} />
       ))}
     </div>
   )
--- frontend/src/components/Tag/TagTile.tsx
+++ frontend/src/components/Tag/TagTile.tsx
@@ -1,8 +1,39 @@
-export const TagTile = () => {
+import { Link } from "react-router-dom"
+import { useAppSelector } from "../../app/hooks"
+import { cleanTagName } from "../../features/tagsSlice"
+import "./TagTile.css"
+import devicon from "./devicon.json"
+
+interface Props {
+  tagId: number
+}
+
+export const TagTile = ({ tagId }: Props) => {
+  const getIconClassName = (name: string) => {
+    const manuallyMappedNames: Record<string, string> = {
+      aws: "amazonwebservices-plain-wordmark",
+      css: "css3-plain",
+      html: "html5-plain",
+      sqlalchemy: "sqlalchemy-plain-wordmark",
+    }
+    if (Object.keys(manuallyMappedNames).includes(name)) {
+      return manuallyMappedNames[name]
+    } else {
+      const icon = devicon.find(icon => icon.name === name)
+      return icon ? [icon.name, icon.versions.font[0]].join("-") : ""
+    }
+  }
+
+  const tag = useAppSelector(state => state.tags.tags)[tagId]
+  const deviconName = getIconClassName(tag.cleanedName)
+  const icon = devicon.find(icon => icon.name === deviconName)
+  const iconName = icon ? [icon.name, icon.versions.font[0]].join("-") : ""
+  // }
+  const iconClass = `devicon-${tag.cleanedName}`
   return (
-    <div>
-      <i className="devicon-devicon-plain"></i>
-      asdf
-    </div>
+    <Link to={`${tag.id}/${tag.cleanedName}`} className="tag-tile">
+      <i className={`devicon-${deviconName} tag-icon`}></i>
+      <div className="tag-text">{tag.name}</div>
+    </Link>
   )
 }

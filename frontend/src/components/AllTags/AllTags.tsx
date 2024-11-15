2999c32d4b707803593006e1a4e7bd39aafd8b52
joshwanf <17016446+joshwanf@users.noreply.github.com>
2024-11-14 19:24:17 -0500

    testing devicon

--- frontend/src/components/AllTags/AllTags.tsx
+++ frontend/src/components/AllTags/AllTags.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { useAppSelector, useAppDispatch } from "../../app/hooks"
 import { selectTags, getAllTags } from "../../features/tagsSlice"
 import { Tag } from "../Tag/Tag"
+import { TagTile } from "../Tag/TagTile"
 export const AllTags = () => {
   const dispatch = useAppDispatch()
   const [isLoadedTags, setIsLoadedTags] = useState(false)
@@ -13,7 +14,10 @@ export const AllTags = () => {
   return (
     <div className="all-tags">
       {tagIds.map(tagId => (
-        <Tag key={tagId} tagId={Number(tagId)} />
+        <div>
+          <TagTile />
+          <Tag key={tagId} tagId={Number(tagId)} />
+        </div>
       ))}
     </div>
   )
---
+++ frontend/src/components/Tag/TagTile.tsx
@@ -0,0 +1,8 @@
+export const TagTile = () => {
+  return (
+    <div>
+      <i className="devicon-devicon-plain"></i>
+      asdf
+    </div>
+  )
+}
